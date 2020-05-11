3adcfdb55782b88e4fb9e0e17246887211b520ff
hfoley <30324719+hfoley@users.noreply.github.com>
2020-05-11 15:49:51 -0400

    Add files via upload

---
+++ AzureSQLScripts/03-InsertRecordsToMetadataLoad.sql
@@ -0,0 +1,29 @@
+/*Insert data into [ADF].[MetadataLoad] table 
+	[FileName] << File to load into Synapse and needs to match the parameter you pass to ADF pipeline 
+   ,[BlobContainer] << ADLS container where parquet file to load exists
+   ,[TargetTableSchema] << schema for target and staging tables 
+   ,[StagingTable] << used for incremental load pipelines in Synapse
+   ,[TargetTable] << final destination table loaded into Synapse
+   ,[ColumnKey] << column used to match the loading from Staging to Target
+
+		   ***REPLACE VALUES BASED ON YOUR ENVIRONMENT ***
+*/ 
+
+INSERT INTO [ADF].[MetadataLoad]
+           ([FileName]
+           ,[BlobContainer]
+           ,[TargetTableSchema]
+           ,[StagingTable]
+           ,[TargetTable]
+           ,[ColumnKey])
+     VALUES
+           ('<parquet filename>'
+           ,'<ADLS container where parquet file stored>'
+		   ,'<schema>'
+           ,'<staging table>'
+           ,'<target table>'
+		   ,'<column key>')
+GO
+
+
+
